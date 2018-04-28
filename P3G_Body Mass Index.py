b28464feff1a20b65c4b67f0872f6c459d6414b6
Tim.MUP <38474993+TimMUP@users.noreply.github.com>
2018-04-28 00:15:20 -0400

    Body Mass Index Question
    This program takes your weight then your height in meters. In which it is then able to determine if the person is either overweight, underweight, or normal weight.

---
+++ P3G_Body Mass Index.py
@@ -0,0 +1,10 @@
+weight = int(input())
+height = float(input())
+
+BMI = weight/ height**2
+if BMI > 25:
+    print("Overweight")
+elif BMI < 18.5:
+    print("Underweight")
+else:
+    print("Normal weight")
