344f798ac40c05b2a0a8f066ea8ede86dcd55e85
Tim.MUP <38474993+TimMUP@users.noreply.github.com>
2018-04-29 21:01:33 -0400

    Cross Country Question
    This program translates sports activities of schools to their full name.

---
+++ P3G_Cross Country.py
@@ -0,0 +1,15 @@
+string = input()
+if string == "MG":
+    print("miget girls")
+elif string == "MB":
+    print("midget boys")
+elif string == "JG":
+    print("junior girls")
+elif string == "JB":
+    print("junior boys")
+elif string == "SG":
+    print("senior girls")
+elif string == "SB":
+    print("senior boys")
+else:
+    print("invalid code")
