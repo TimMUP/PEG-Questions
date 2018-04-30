b3943a1262b76a2f3a80f6d3391bf6a7dd9b8b2e
Tim.MUP <38474993+TimMUP@users.noreply.github.com>
2018-04-29 21:10:33 -0400

    I Speak TXTMSG Question
    This program takes the input of a text message shortened form and converts it into the full standard meaning. See PEG for more information.

---
+++ P3G_I Speak TXTMSG.py
@@ -0,0 +1,29 @@
+while True:
+    ans = input()
+    if ans == "CU":
+        print("see you")
+    elif ans == ":-)":
+        print("I'm happy")
+    elif ans == ":-(":
+        print("I'm unhappy")
+    elif ans == ";-)":
+        print("wink")
+    elif ans == ":-P":
+        print("stick out my tongue")
+    elif ans == "(~.~)":
+        print("sleepy")
+    elif ans == "TA":
+        print("totally awesome")
+    elif ans == "CCC":
+        print("Canadian Computing Competition")
+    elif ans == "CUZ":
+        print("because")
+    elif ans == "TY":
+        print("thank-you")
+    elif ans == "YW":
+        print("you're welcome")
+    elif ans == "TTYL":
+        print("talk to you later")
+        break
+    else:
+        print(ans)
